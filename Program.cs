65fedc630e588f692be14f112359a02f77ea09c1
Aquinnos <wellandcomeu@gmail.com>
2023-11-01 16:58:05 +0100

    dodanie formularzy rejestracji i logowania i wiele innych rzeczy :D

--- Program.cs
+++ Program.cs
@@ -1,59 +1,259 @@
 ﻿using System;
+using System.Threading;
 using Microsoft.Data.Sqlite;
+using System.Media;
 
-internal class Program
+namespace govApp
 {
-    private static void Main(string[] args)
+    public class Authentication
     {
-        // Utwórz instancję DatabaseConnector i podaj ścieżkę do bazy danych SQLite
-        DatabaseConnector dbConnector = new DatabaseConnector("./govapp.sqlite");
+        private SqliteConnection _connection;
 
-        // Otwórz połączenie z bazą danych
-        dbConnector.OpenConnection();
-
-        while (true)
+        public Authentication(SqliteConnection connection)
         {
-            Console.WriteLine("Wybierz opcję:");
-            Console.WriteLine("1. Dostępne Programy");
-            Console.WriteLine("2. Moje Wnioski");
-            Console.WriteLine("3. Profil");
-            Console.WriteLine("4. Pomoc i obsługa"); 
-            Console.WriteLine("5. Wyjście");
+            _connection = connection;
+        }
 
-            string userInput = Console.ReadLine();
-
-            if (userInput != null)
+        public bool Login(string username, string password)
+        {
+            using (var cmd = _connection.CreateCommand())
             {
-                switch (userInput)
+                cmd.CommandText = "SELECT UserId FROM users WHERE Username = @username AND Password = @password";
+                cmd.Parameters.AddWithValue("@username", username);
+                cmd.Parameters.AddWithValue("@password", password);
+
+                using (var reader = cmd.ExecuteReader())
                 {
-                    case "1":
-                        Console.WriteLine("Przechodzisz do Dostępnych Programów"); // funkcja programów
-                        break;
-                    case "2":
-                        Console.WriteLine("Przechodzisz do Moich Wniosków"); // funkcja wniosków
-                        break;
-                    case "3":
-                        Console.WriteLine("Przechodzisz do Profilu"); // funkcja profilu użytkownika
-                        break;
-                    case "4":
-                        Console.WriteLine("Przechodzisz do Pomocy i Obsługi"); // funkcja pomocy i obsługi (może jakieś FAQ)
-                        break;
-                    case "5":
-                        Console.WriteLine("Zamykanie aplikacji."); // funkcja
+                    if (reader.Read())
+                    {
+                        // zwracamy true kiedy znaleziono użytkownika
+                        return true;
+                    }
+                }
 
-                        // Zamknij połączenie z bazą danych przed zakończeniem programu
-                        dbConnector.CloseConnection();
+                // zwracany false kiedy nie znaleziono użytkownika
+                return false;
+            }
+        }
 
-                        return;
-                    default:
-                        Console.WriteLine("Niepoprawna opcja. Wybierz jeszcze raz."); // funkcja
-                        break;
+        public bool Register(string username, string password, string firstName, string lastName)
+        {
+            using (var cmd = _connection.CreateCommand())
+            {
+                cmd.CommandText = "INSERT INTO users (Username, Password, FirstName, LastName) VALUES (@username, @password, @firstName, @lastName)";
+                cmd.Parameters.AddWithValue("@username", username);
+                cmd.Parameters.AddWithValue("@password", password);
+                cmd.Parameters.AddWithValue("@firstName", firstName);
+                cmd.Parameters.AddWithValue("@lastName", lastName);
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+
+                // zwracamy true jeśli użytkownik został dodany poprawnie
+                return rowsAffected > 0;
+            }
+        }
+    }
+
+
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            DatabaseConnector dbConnector = new DatabaseConnector("./govapp.sqlite");
+            dbConnector.OpenConnection();
+            Authentication authentication = new Authentication(dbConnector.GetConnection());
+
+            ConsoleKeyInfo keyInfo;
+            int selectedOption = 0;
+            string[] menuOptions = { "Logowanie", "Rejestracja", "Wejście bez logowania", "Dostępne Programy", "Moje Wnioski", "Profil", "Pomoc i Obsługa", "Wyjście" };
+            bool isLoggedIn = false;
+            string username = "";
+
+            SoundPlayer scrollSound = new SoundPlayer("./sounds/scroll.wav");
+            SoundPlayer selectSound = new SoundPlayer("./sounds/select.wav");
+
+            string asciiArt = @"
+                     ___              
+   ____ _____ _   __/   |  ____  ____ 
+  / __ `/ __ \ | / / /| | / __ \/ __ \
+ / /_/ / /_/ / |/ / ___ |/ /_/ / /_/ /
+ \__, /\____/|___/_/  |_/ .___/ .___/ 
+/____/                 /_/   /_/      
+        ";
+
+            Console.WriteLine(asciiArt);
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Witaj w Aplikacji Rządowej!");
+            Console.WriteLine("================================");
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Aplikacja Rządowa to narzędzie do dostępu do różnych programów i formularzy rządowych.");
+            Thread.Sleep(1000);
+            Console.WriteLine("Możesz korzystać z dostępnych programów, składać wnioski oraz zarządzać swoim profilem.");
+            Thread.Sleep(1000);
+            Console.WriteLine("Aby rozpocząć, wybierz jedną z dostępnych opcji z poniższego menu.");
+            Thread.Sleep(3000);
+            Console.WriteLine("\n");
+            Console.Clear();
+
+            while (!isLoggedIn)
+            {
+                Console.Clear();
+                Console.WriteLine(asciiArt);
+                Console.WriteLine("\n");
+                Console.WriteLine("Logowanie lub Rejestracja:");
+                for (int i = 0; i < 3; i++)
+                {
+                    if (i == selectedOption)
+                    {
+                        Console.Write(">> ");
+                    }
+                    Console.WriteLine($"{i + 1}. {menuOptions[i]}");
+                }
+
+                keyInfo = Console.ReadKey();
+                if (keyInfo.Key == ConsoleKey.Enter)
+                {
+                    if (selectedOption == 0)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Logowanie:");
+                        Console.Write("Podaj nazwę użytkownika: ");
+                        username = Console.ReadLine();
+                        Console.Write("Podaj hasło: ");
+                        string password = Console.ReadLine();
+
+                        if (authentication.Login(username, password))
+                        {
+                            isLoggedIn = true;
+                            Console.WriteLine("Zalogowano pomyślnie!");
+                            Thread.Sleep(1000);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Błąd logowania. Spróbuj ponownie.");
+                            Thread.Sleep(1000);
+                        }
+                    }
+                    else if (selectedOption == 1)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Rejestracja:");
+                        Console.Write("Podaj nazwę użytkownika: ");
+                        username = Console.ReadLine();
+                        Console.Write("Podaj hasło: ");
+                        string password = Console.ReadLine();
+                        Console.Write("Podaj imię: ");
+                        string firstName = Console.ReadLine();
+                        Console.Write("Podaj nazwisko: ");
+                        string lastName = Console.ReadLine();
+
+                        // sprawdzamy czy pola nie są puste
+                        if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password) || string.IsNullOrWhiteSpace(firstName) || string.IsNullOrWhiteSpace(lastName))
+                        {
+                            Console.WriteLine("Wszystkie pola są wymagane. Spróbuj ponownie.");
+                            Thread.Sleep(1000);
+                        }
+                        else
+                        {
+                            if (authentication.Register(username, password, firstName, lastName))
+                            {
+                                Console.WriteLine("Zarejestrowano pomyślnie!");
+                                Thread.Sleep(1000);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Błąd rejestracji. Spróbuj ponownie.");
+                                Thread.Sleep(1000);
+                            }
+                        }
+                    }
+                    else if (selectedOption == 2)
+                    {
+                        // wejście bez logowania
+                        isLoggedIn = true; // użytkownik zalogowany, nie mając konta
+                        username = "Gość"; // domyślna nazwa użytkownika dla wejścia bez logowania
+                    }
+
+                    Console.WriteLine("Naciśnij dowolny klawisz, aby kontynuować...");
+                    Console.ReadKey();
+                }
+                else if (keyInfo.Key == ConsoleKey.UpArrow)
+                {
+                    selectedOption = Math.Max(0, selectedOption - 1);
+                    scrollSound.Play();
+                }
+                else if (keyInfo.Key == ConsoleKey.DownArrow)
+                {
+                    selectedOption = Math.Min(2, selectedOption + 1);
+                    scrollSound.Play();
                 }
             }
-            else
+
+            // głowna pętla menu
+            do
             {
-                Console.WriteLine("Wystąpił problem z odczytem danych. Spróbuj jeszcze raz.");
-            }
+                Console.Clear();
+                Console.WriteLine(asciiArt);
+                Console.WriteLine("\n");
+                Console.WriteLine($"Zalogowano jako: {username}");
+                for (int i = 3; i < menuOptions.Length; i++)
+                {
+                    if (i == selectedOption)
+                    {
+                        Console.Write(">> ");
+                    }
+                    Console.WriteLine($"{i - 3 + 1}. {menuOptions[i]}");
+                }
+
+                keyInfo = Console.ReadKey();
+
+                if (keyInfo.Key == ConsoleKey.Enter)
+                {
+                    switch (selectedOption)
+                    {
+                        case 3:
+                            Console.WriteLine("Przechodzisz do Dostępnych Programów.");
+                            selectSound.Play();
+                            // funkcja dostępnych programów
+                            break;
+                        case 4:
+                            Console.WriteLine("Przechodzisz do Moich Wniosków.");
+                            selectSound.Play();
+                            // funkcja wniosków
+                            break;
+                        case 5:
+                            Console.WriteLine("Przechodzisz do Profilu.");
+                            selectSound.Play();
+                            // funkcja profilu
+                            break;
+                        case 6:
+                            Console.WriteLine("Przechodzisz do Pomocy i Obsługi.");
+                            selectSound.Play();
+                            // funkcja pomocy
+                            break;
+                        case 7:
+                            Console.WriteLine("Zamykanie aplikacji.");
+                            return;
+                    }
+
+                    Console.WriteLine("Naciśnij dowolny klawisz, aby kontynuować...");
+                    Console.ReadKey();
+                }
+                else if (keyInfo.Key == ConsoleKey.UpArrow)
+                {
+                    selectedOption = Math.Max(2, selectedOption - 1);
+                    scrollSound.Play();
+                }
+                else if (keyInfo.Key == ConsoleKey.DownArrow)
+                {
+                    selectedOption = Math.Min(menuOptions.Length - 1, selectedOption + 1);
+                    scrollSound.Play();
+                }
+
+            } while (keyInfo.Key != ConsoleKey.Escape);
         }
     }
 }
